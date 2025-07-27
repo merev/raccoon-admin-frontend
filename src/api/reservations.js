0d1204f3f8958e2d13c27278199c38b1e9053595
merev <merev.personal@gmail.com>
2025-07-27 15:42:25 +0300

    add pagination

--- src/api/reservations.js
+++ src/api/reservations.js
@@ -3,13 +3,16 @@ import axios from "axios";
 const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
 
 export const fetchReservations = async (params = {}) => {
-  // Clean up params by removing empty values
   const cleanParams = Object.fromEntries(
     Object.entries(params).filter(([_, v]) => v !== undefined && v !== '')
   );
   
   const res = await axios.get(`${API_BASE}/admin/reservations`, { 
-    params: cleanParams 
+    params: {
+      ...cleanParams,
+      page: params.page || 1,
+      per_page: params.per_page || 10
+    }
   });
   return res.data;
 };
